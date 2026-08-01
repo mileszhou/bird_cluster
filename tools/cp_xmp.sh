79e4ac7d9e21ae3f622c62d6b7d8525ba435cb95
Miles Zhou <mileszhou@gmail.com>
2026-08-02 07:21:16 +0800

    Add more photo library copy/backup scripts.

--- tools/cp_xmp.sh
+++ tools/cp_xmp.sh
@@ -7,3 +7,25 @@ rsync -av --prune-empty-dirs \
     --exclude='*' \
     Photos/Photos-{18..25} \
     "spark:$HOME/projects/bird_cluster/data/xmp0/"
+
+# Copy the photo library
+rsync -av \
+    --include='Photos-18/***' \
+    --include='Photos-19/***' \
+    --include='Photos-20/***' \
+    --include='Photos-21/***' \
+    --include='Photos-22/***' \
+    --include='Photos-23/***' \
+    --include='Photos-24/***' \
+    --include='Photos-25/***' \
+    --exclude='*' \
+    Photos/ \
+    "kepler:/mnt/d/Lightroom/MediaFiles/Photos"
+
+#Backup the photo library before mvong photos around
+rsync -av --prune-empty-dirs \
+    --include='*/' \
+    --include='*.xmp' \
+    --exclude='*' \
+    Photos/Photos-{18..25} \
+    "n4:/syno/Lightroom/MediaFiles/Photos/"
