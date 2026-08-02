e68a4e332d62ede1c7c07d458e489ab58287b01e
Miles Zhou <mileszhou@gmail.com>
2026-08-02 14:39:06 +0800

    Finished dedup and added a script to copy xml files.

--- tools/cp_xmp.sh
+++ tools/cp_xmp.sh
@@ -5,6 +5,7 @@ rsync -av --prune-empty-dirs \
     --include='*/' \
     --include='*.xmp' \
     --exclude='*' \
+    --delete \
     Photos/Photos-{18..25} \
     "localhost:$HOME/projects/bird_cluster/data/xmp0/"
 
