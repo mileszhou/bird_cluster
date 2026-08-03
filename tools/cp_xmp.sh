b8e856b96ac7b4ab366efa9a2da356e565ab35d8
Miles Zhou <mileszhou@gmail.com>
2026-08-03 21:47:20 +0800

    Add data export report.

--- tools/cp_xmp.sh
+++ tools/cp_xmp.sh
@@ -7,7 +7,7 @@ rsync -av --prune-empty-dirs \
     --exclude='*' \
     --delete \
     Photos/Photos-{18..25} \
-    "localhost:$HOME/projects/bird_cluster/data/xmp0/"
+    "localhost:$HOME/projects/bird_cluster/data/xmp/"
 
 # Copy the photo library
 rsync -av \
